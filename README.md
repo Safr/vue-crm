871c73d21430f81162aaa2d8dc8fd2d10f3fcd82
dpo3etka <djpo3etka@gmail.com>
2019-07-22 20:22:14 +0300

    Deploy project

---
+++ README.md
@@ -0,0 +1,48 @@
+# VUE CRM
+
+## Demo
+
+[Live DEMO](https://vue-crm-54f37.firebaseapp.com/)
+
+## The project was build using the following technologies:
+
+- Vue
+- Vue Router
+- Vuex
+- Materialize
+
+## Features
+
+- List income (outcome)
+- Authorization with Firebase
+- Create & Edit your outcome and income
+- Simple statistics of your budget
+- Custom localization (RU/ENG)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/main.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import Paginate from 'vuejs-paginate'
+import VueMeta from 'vue-meta'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
+import localizeFilter from '@/filters/localize.filter'
+import tooltipDirective from '@/directives/tooltip.directive'
+import messagePlugin from '@/utils/message.plugin'
+import titlePlugin from '@/utils/title.plugin'
+import Loader from '@/components/app/Loader'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+Vue.config.productionTip = false
+
+Vue.use(messagePlugin)
+Vue.use(titlePlugin)
+Vue.use(Vuelidate)
+Vue.use(VueMeta)
+Vue.filter('date', dateFilter)
+Vue.filter('localize', localizeFilter)
+Vue.filter('currency', currencyFilter)
+Vue.directive('tooltip', tooltipDirective)
+Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBM46TDePU8N38LAtLeqiF_kJ9NF192kOs",
+  authDomain: "vue-crm-54f37.firebaseapp.com",
+  databaseURL: "https://vue-crm-54f37.firebaseio.com",
+  projectId: "vue-crm-54f37",
+  storageBucket: "",
+  messagingSenderId: "770094462628",
+  appId: "1:770094462628:web:ea830d1302789642"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
