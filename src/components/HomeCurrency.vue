2926a3e63ddcc6f0a7ff4f321e017ffcdda71ecb
dpo3etka <djpo3etka@gmail.com>
2019-07-23 11:54:32 +0300

    Fix(api): change api endpoint to work on https

--- src/components/HomeCurrency.vue
+++ src/components/HomeCurrency.vue
@@ -17,7 +17,7 @@
           <tbody>
             <tr v-for="cur in currencies" :key="cur">
               <td>{{cur}}</td>
-              <td>{{rates[cur].toFixed(5)}}</td>
+              <td>{{rates[cur].toFixed(2)}}</td>
               <td>{{date | date('date')}}</td>
             </tr>
           </tbody>
